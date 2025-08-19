c45fa31d28b7d802166ef0d5b535edf7ded4e7c6
Mounir Musbah <moneerjoda@gmail.com>
2025-08-19 13:11:22 +0300

    تجهيز ملفات الموقع

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Al-akaria</title>
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="assets/css/fancybox.css"/>
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+
+
+
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <script src="assets/js/fancybox.umd.js"></script>
+    <script>
+      Fancybox.bind('[data-fancybox]', {});    
+    </script>
+
+  </body>
+</html>
