4c272b7451f68c4c705648953be26105c9d324f7
Mounir Musbah <moneerjoda@gmail.com>
2025-08-20 10:37:39 +0300

    home
    code for home

--- index.php
+++ index.php
@@ -12,6 +12,148 @@
   </head>
   <body>
 
+  <header class="header">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand me-5" href="#"> <img src="assets/images/logo.svg"> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                نبذة عن العقارية
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">نبذة عن العقارية</a></li>
+                <li><a class="dropdown-item" href="#">الرؤية والرسالة</a></li>
+                <li><a class="dropdown-item" href="#">كلمة الرئيس التنفيذي</a></li>
+                <li><a class="dropdown-item" href="#">أعضاء مجلس الإدارة</a></li>
+                <li><a class="dropdown-item" href="#">الشركات التابعة</a></li>
+                <li><a class="dropdown-item" href="#">تاريخ العقارية</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">خدماتنا</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">المشاريع</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">المستثمرون</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">التغطية الإعلامية</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                الموردين
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">تسجيل كمورد جديد</a></li>
+                <li><a class="dropdown-item" href="#">تسجيل الدخول</a></li>
+                <li><a class="dropdown-item" href="#">دليل بوابة الموردين</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">التوظيف</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="btn btn-light me-2" href="#">EN</a></li>
+            <li class="nav-item"><a class="btn btn-light me-2 search-btn" href="#"><img src="assets/images/search.png"></a></li>
+            <li class="nav-item"><a class="btn btn-primary" href="#">تواصل معنا</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <section class="hero">
+    <video>
+      <source src="movie.mp4" type="video/mp4">
+      <source src="movie.ogg" type="video/ogg">
+      Your browser does not support the video tag.
+    </video>
+  </section>
+
+  <section class="about-block py-80">
+    <div class="container">
+      <div class="about-us mb-5">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="title-block">
+              <h2>مسيرة عريقة</h2>
+              <h3>وطموحات مستقبلية</h3>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <p>منذ تأسيسها في 15 يوليو 1976 بموجب مرسوم ملكي وإدراجها كشركة مساهمة في سوق الأسهم السعودية، تواصل الشركة العقارية مسيرتها الرائدة في البناء والتطوير. بفضل رؤية واضحة للمستقبل، واصلت الشركة تحقيق إنجازات نوعية، متماشيةً مع رؤية المملكة العربية السعودية، حيث أسهمت في تطوير مجتمعات عصرية ومستدامة من خلال مشاريعها الرائدة.</p>
+            <p>يستحوذ صندوق الاستثمارات العامة على 64.57٪ من أسهم الشركة، بينما يمتلك المساهمون الآخرون نسبة 35.43٪، دون أن تتجاوز حصة أي منهم 5٪. هذا التوزيع يمكّن الشركة من تحقيق طموحاتها في مواصلة الابتكار وتطوير أسلوب حياة عقاري متجدد.</p>
+          </div>
+        </div>
+      </div>
+      <div class="gallery-about d-flex justify-content-center gap-1">
+        <a href="#"><img src="assets/images/about1.webp"></a>
+        <a href="#"><img src="assets/images/about2.webp"></a>
+        <a href="#"><img src="assets/images/about3.webp"></a>
+      </div>
+      <div class="counter d-flex justify-content-center mt-4 mb-5 gap-5 text-center">
+        <div class="counter-block">
+          <label>09</label>
+          <h4>مشاريع جاهزة</h4>
+        </div>
+        <div class="counter-block">
+          <label>+3.75مليار</label>
+          <h4>ريال سعودي رأس مال مدفوع</h4>
+        </div>
+        <div class="counter-block">
+          <label>+26مليون</label>
+          <h4>متر مربع من الأراضي</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="offset-md-4 col-md-8">
+          <div class="about-services">
+            <h3>مواقع استراتيجية:</h3>
+            <p>تقدم "العقارية" فرصًا استثمارية فريدة بمواقعها الاستراتيجية المثالية لبناء مبانٍ سكنية وتجارية. تناسب هذه المواقع الأفراد الباحثين عن سكن مريح والشركات التي ترغب في إنشاء مساحات تجارية نابضة بالحياة، مما يعزز من التوسع والنمو.</p>
+          </div>
+          <div class="about-services">
+            <h3>تصاميم عصرية:</h3>
+            <p>تستند "العقارية" إلى خبرة تمتد لأكثر من خمسة عقود لتقديم تصاميم عصرية مبتكرة تتماشى مع التوجهات الحديثة. تهدف هذه التصاميم إلى تلبية احتياجات الأفراد والشركات، مع ضمان التوازن بين الجمالية والوظيفية لخلق بيئات معيشية وعمل مثالية.</p>
+          </div>
+          <div class="about-services">
+            <h3>أمان واستدامة:</h3>
+            <p>"تسعى ""العقارية"" لتحقيق أعلى معايير الأمان والاستدامة في مشاريعها، مع الالتزام بتجاوز توقعات العملاء. من خلال أساليب مبتكرة وتقنيات حديثة، تضمن الشركة بيئات آمنة ومستدامة، مما يحسن جودة الحياة ويعزز قيمة الاستثمارات.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="projects py-80 pb-5 mx-2">
+      <div class="container title-block mb-5">
+        <h2>ماضٍ عريق يلهم مستقبلنا</h2>
+        <h3>رؤية تتجلى في كل مشروع ننجزه</h3>
+      </div>
+    <div class="projects-slider owl-carousel">
+      <div class="card-project position-relative">
+        <label class="type-project">بيع</label>
+        <img class="img-fluid img-project" src="assets/images/project.webp">
+        <div class="info-project text-white p-4">
+          <label class="d-flex align-items-center gap-2 mb-3"><img src="assets/images/location.svg"> الرياض</label>
+          <h2 class="mb-3">العقارية الثانية</h2>
+          <span>تجاري - مكتبي</span>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
 
 
   <script src="assets/js/bootstrap.bundle.min.js"></script>
@@ -21,6 +163,39 @@
     <script>
       Fancybox.bind('[data-fancybox]', {});    
     </script>
+
+<script>
+    $('.projects-slider').owlCarousel({
+      loop: true,
+      margin: 7,
+      autoplayTimeout: 6000,
+      smartSpeed: 1500,
+      nav: false,
+      navText: ['<img src="images/arrow-right.svg">', '<img src="images/arrow-left.svg">'],
+      lazyLoad: true,
+      rtl: true,
+      autoplay: true,
+      dots: false,
+      responsive: {
+          0: {
+              items: 1
+          },
+          450: {
+              items: 2
+          },
+          600: {
+              items: 3
+          },
+          992: {
+              items: 4
+          },
+          1000: {
+              items: 4
+          }
+      }
+  });
+
+</script>
 
   </body>
 </html>
